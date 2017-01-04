f86a018536b4c8ea2ff341582cccc87658f02db5
Lyuwen Fu <fulvwen@gmail.com>
2017-01-03 20:25:23 -0500

    updated intro

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsync -a --exclude=".DS_Store" --exclude=".git" --exclude=".gitignore" ../personal-v2/ web:public_html/me
+#rsync -a --exclude=".DS_Store" --exclude=".git" --exclude=".gitignore" ../personal-v2/ web:public_html/me
+rsync -a --exclude=".DS_Store" --exclude=".git" --exclude=".gitignore" ../personal-v2/ web:public_html
